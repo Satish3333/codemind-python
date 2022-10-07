b6b1c1dd2b8fd7acce8f97c4b71aaea501801d0a
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-10-07 19:45:02 +0545

    n=int(input()) l=[]
    for i in range(1,n+1):
        l.append(2**i)
    t=n
    for i in range(n,0,-1):
        if i in l:
            t1=i
            break
    while True:
        if t in l:
            t2=t
            break
        else:
            t=t+1
    if t1==t2:
        print(0)
    else:
        d1=n-t1
        d2=t2-n
    print(min(d1,d2))

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.py
@@ -0,0 +1,22 @@
+n=int(input())
+l=[]
+
+for i in range(1,n+1):
+    l.append(2**i)
+t=n
+for i in range(n,0,-1):
+    if i in l:
+        t1=i
+        break
+while True:
+    if t in l:
+        t2=t
+        break
+    else:
+        t=t+1
+if t1==t2:
+    print(0)
+else:
+    d1=n-t1
+    d2=t2-n
+print(min(d1,d2))

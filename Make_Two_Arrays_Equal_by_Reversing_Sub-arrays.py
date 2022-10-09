648860cec7cccf82836d3bf4a5c685adaf076fa4
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-10-09 15:55:55 +0545

    a=int(input()) l1=list(map(int,input().split())) b=int(input()) l2=list(map(int,input().split())) for i in l1:     continue if i not in l2:     print(False) else:     print("True")

---
+++ Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.py
@@ -0,0 +1,10 @@
+a=int(input())
+l1=list(map(int,input().split()))
+b=int(input())
+l2=list(map(int,input().split()))
+for i in l1:
+    continue
+if i not in l2:
+    print(False)
+else:
+    print("True")

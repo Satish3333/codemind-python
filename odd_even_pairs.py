6362b9efa5298d732f85b0d599c34d2a7908d9f5
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-11-03 14:50:46 +0545

    def fun(l,k):     l1=[]     for i in range(len(l)):         l1.append(l[i])         l1.append(k[i])     return l1 n=int(input()) k=list(map(int,input().split())) e=[] o=[] for i in k:     if i%2==1:         e.append(i)     else:         o.append(i) if len(e)<len(o):     for j in range(len(o)-len(e)):         e.append("#") else:     for j in range(len(e)-len(o)):         o.append("#") l1=fun(e,o) l2=[] for i in l1:     if i!="#":         l2.append(i) if n%2==1:     l2.append(0)     print(*l2) else:     print(*l2)

---
+++ odd_even_pairs.py
@@ -0,0 +1,31 @@
+def fun(l,k):
+    l1=[]
+    for i in range(len(l)):
+        l1.append(l[i])
+        l1.append(k[i])
+    return l1
+n=int(input())
+k=list(map(int,input().split()))
+e=[]
+o=[]
+for i in k:
+    if i%2==1:
+        e.append(i)
+    else:
+        o.append(i)
+if len(e)<len(o):
+    for j in range(len(o)-len(e)):
+        e.append("#")
+else:
+    for j in range(len(e)-len(o)):
+        o.append("#")
+l1=fun(e,o)
+l2=[]
+for i in l1:
+    if i!="#":
+        l2.append(i)
+if n%2==1:
+    l2.append(0)
+    print(*l2)
+else:
+    print(*l2)

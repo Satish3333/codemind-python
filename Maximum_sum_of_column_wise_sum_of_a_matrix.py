2833680ebb4e16d4a1630763f991fd9fd6173764
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-11-08 21:46:51 +0545

    a,b=map(int,input().split()) l1=[] for i in range(a):     l=list(map(int,input().split()))     l1.append(l) k=[] for i in range(b):     a=0     for j in range(len(l1)):         a=a+l1[j][i]         k.append(a) print(max(k))

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+l1=[]
+for i in range(a):
+    l=list(map(int,input().split()))
+    l1.append(l)
+k=[]
+for i in range(b):
+    a=0
+    for j in range(len(l1)):
+        a=a+l1[j][i]
+        k.append(a)
+print(max(k))
+    
